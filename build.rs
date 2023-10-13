825af6dd55afc3cb56b029e875cbea96eab1a114
Cliff Wu <z800201002005@gmail.com>
2023-10-13 15:55:55 +0900

    Display current directory and binary location

--- build.rs
+++ build.rs
@@ -2,12 +2,14 @@
 
 // Standard
 use std::env;
+use std::path::Path;
 
 // Constant
 // https://doc.rust-lang.org/cargo/reference/environment-variables.html
 const ENV_CARGO_CFG_TARGET_ARCH: &str = "CARGO_CFG_TARGET_ARCH";
 const ENV_CARGO_CFG_TARGET_OS: &str = "CARGO_CFG_TARGET_OS";
 const ENV_PROFILE: &str = "PROFILE";
+const ENV_OUT_DIR: &str = "OUT_DIR";
 
 macro_rules! cargo_warn{
     ($($tokens: tt)*) => {
@@ -19,7 +21,20 @@ fn main() {
     let profile = env::var(ENV_PROFILE).unwrap();
     let arch = env::var(ENV_CARGO_CFG_TARGET_ARCH).unwrap();
     let os = env::var(ENV_CARGO_CFG_TARGET_OS).unwrap();
+    let current_dir = env::current_dir().unwrap();
+    let out_dir = env::var(ENV_OUT_DIR).unwrap();
+    let bin_location = Path::new(&out_dir)
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .display()
+        .to_string();
+    cargo_warn!("Current directory: {:}", current_dir.display());
     cargo_warn!("Profile: {:}", profile);
-    cargo_warn!("Target Arch: {:}", arch);
+    cargo_warn!("Target architecture: {:}", arch);
     cargo_warn!("Target OS: {:}", os);
+    cargo_warn!("Binary location: {:}", bin_location);
 }
