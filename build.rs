841d67488e7626f5b68f03c0b4af09c2c77e4b0a
Cliff Wu <z800201002005@gmail.com>
2023-10-13 14:28:52 +0900

    Show target architecture and OS when build

--- build.rs
+++ build.rs
@@ -4,6 +4,9 @@
 use std::env;
 
 // Constant
+// https://doc.rust-lang.org/cargo/reference/environment-variables.html
+const ENV_CARGO_CFG_TARGET_ARCH: &str = "CARGO_CFG_TARGET_ARCH";
+const ENV_CARGO_CFG_TARGET_OS: &str = "CARGO_CFG_TARGET_OS";
 const ENV_PROFILE: &str = "PROFILE";
 
 macro_rules! cargo_warn{
@@ -14,5 +17,9 @@ macro_rules! cargo_warn{
 
 fn main() {
     let profile = env::var(ENV_PROFILE).unwrap();
-    cargo_warn!("{:}: {:}", ENV_PROFILE, profile);
+    let arch = env::var(ENV_CARGO_CFG_TARGET_ARCH).unwrap();
+    let os = env::var(ENV_CARGO_CFG_TARGET_OS).unwrap();
+    cargo_warn!("Profile: {:}", profile);
+    cargo_warn!("Target Arch: {:}", arch);
+    cargo_warn!("Target OS: {:}", os);
 }
