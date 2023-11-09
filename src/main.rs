aaadc71a828f264fff35f7b49303fcba8d7b86a5
Cliff Wu <z800201002005@gmail.com>
2023-11-09 18:16:33 +0900

    Use logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+// crates.io
+use tracing::{debug, error, info, trace, warn};
+
+// Custom library
+use rust_template::logger;
+
 fn main() {
-    println!("Hello, world!");
+    logger::init();
+    trace!("Hello, world!");
+    debug!("Hello, world!");
+    info!("Hello, world!");
+    warn!("Hello, world!");
+    error!("Hello, world!");
 }
