512c579f7f528789b0a51fddbcbca6419481c44d
Cliff Wu <z800201002005@gmail.com>
2023-11-13 17:11:47 +0900

    Create asynchronous main function with Tokio

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 // crates.io
+use tokio::runtime;
 use tracing::{debug, error, info, trace, warn};
 
 // Custom library
 use rust_template::logger;
+
+async fn main_async() {
+    trace!("Hello, world!");
+    debug!("Hello, world!");
+    info!("Hello, world!");
+    warn!("Hello, world!");
+    error!("Hello, world!");
+}
 
 fn main() {
     /*
@@ -11,9 +20,9 @@ fn main() {
      * This will ensure that the guard will be dropped during an unwinding or when main exits successfully
      */
     let _ground = logger::init();
-    trace!("Hello, world!");
-    debug!("Hello, world!");
-    info!("Hello, world!");
-    warn!("Hello, world!");
-    error!("Hello, world!");
+    runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(main_async());
 }
