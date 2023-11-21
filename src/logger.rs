decac2f89a5bffaeaaeb987a45515cb55b1ca885
Cliff Wu <z800201002005@gmail.com>
2023-11-21 17:19:57 +0900

    Change the logger to be object-oriented

--- src/logger.rs
+++ src/logger.rs
@@ -14,54 +14,75 @@ use tracing_subscriber::{
     filter::LevelFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt, Layer,
 };
 
-pub fn init() -> non_blocking::WorkerGuard {
-    let format = "[year]-[month]-[day] [hour]:[minute]:[second].[subsecond digits:3]";
-    let timer = OffsetTime::new(
-        UtcOffset::current_local_offset().unwrap(),
-        format_description::parse(format).unwrap(),
-    );
+pub struct Logger {
+    pub console_level: Level,
+    pub file_level: Level,
+    pub file_path: String,
+}
 
-    // TODO: parameterization
-    let file_level = Level::WARN;
-    let console_level = Level::INFO;
-    let file_path = "log/rust_template.log";
+impl Logger {
+    pub fn new(console_level: Level, file_level: Level, file_path: String) -> Self {
+        Self {
+            console_level,
+            file_level,
+            file_path,
+        }
+    }
 
-    let directory = Path::new(file_path).parent().unwrap();
-    let file_name_prefix = Path::new(file_path).file_name().unwrap();
-    let file_appender = rolling::daily(directory, file_name_prefix);
-    let (non_blocking_appender, _guard) = non_blocking(file_appender);
-    let file_layer = fmt::layer()
-        .with_ansi(false)
-        .with_file(true)
-        .with_line_number(true)
-        .with_timer(timer.clone())
-        .with_thread_names(true)
-        .with_writer(non_blocking_appender)
-        .with_filter(LevelFilter::from_level(file_level));
+    pub fn init(&self) -> non_blocking::WorkerGuard {
+        let format = "[year]-[month]-[day] [hour]:[minute]:[second].[subsecond digits:3]";
+        let timer = OffsetTime::new(
+            UtcOffset::current_local_offset().unwrap(),
+            format_description::parse(format).unwrap(),
+        );
 
-    let stdeer_layer = fmt::layer()
-        .with_ansi(true)
-        .with_file(true)
-        .with_line_number(true)
-        .with_timer(timer.clone())
-        .with_thread_names(true)
-        .with_writer(io::stderr)
-        .with_filter(LevelFilter::from_level(console_level));
+        let directory = Path::new(&self.file_path).parent().unwrap();
+        let file_name_prefix = Path::new(&self.file_path).file_name().unwrap();
+        let file_appender = rolling::daily(directory, file_name_prefix);
+        let (non_blocking_appender, _guard) = non_blocking(file_appender);
+        let file_layer = fmt::layer()
+            .with_ansi(false)
+            .with_file(true)
+            .with_line_number(true)
+            .with_timer(timer.clone())
+            .with_thread_names(true)
+            .with_writer(non_blocking_appender)
+            .with_filter(LevelFilter::from_level(self.file_level));
 
-    tracing_subscriber::Registry::default()
-        .with(file_layer)
-        .with(stdeer_layer)
-        .init();
+        let stdeer_layer = fmt::layer()
+            .with_ansi(true)
+            .with_file(true)
+            .with_line_number(true)
+            .with_timer(timer.clone())
+            .with_thread_names(true)
+            .with_writer(io::stderr)
+            .with_filter(LevelFilter::from_level(self.console_level));
 
-    // https://docs.rs/tracing-panic/0.1.1/tracing_panic/fn.panic_hook.html
-    panic::set_hook(Box::new(panic_hook));
+        tracing_subscriber::Registry::default()
+            .with(file_layer)
+            .with(stdeer_layer)
+            .init();
 
-    info!(
-        console_level = console_level.to_string(),
-        file_level = file_level.to_string(),
-        "Logger initialized:"
-    );
+        // https://docs.rs/tracing-panic/0.1.1/tracing_panic/fn.panic_hook.html
+        panic::set_hook(Box::new(panic_hook));
 
-    // https://docs.rs/tracing-appender/latest/tracing_appender/non_blocking/struct.WorkerGuard.html
-    _guard
+        info!(
+            console_level = self.console_level.to_string(),
+            file_level = self.file_level.to_string(),
+            "Logger initialized:"
+        );
+
+        // https://docs.rs/tracing-appender/latest/tracing_appender/non_blocking/struct.WorkerGuard.html
+        _guard
+    }
+}
+
+impl Default for Logger {
+    fn default() -> Self {
+        Self::new(
+            Level::INFO,
+            Level::WARN,
+            "log/rust_template.log".to_string(),
+        )
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use tokio::sync::broadcast;
 use rust_template::configs;
 use rust_template::constant;
 use rust_template::error::ErrorCode;
-use rust_template::logger::{self, *}; // debug, error, info, trace, warn
+use rust_template::logger::*; // debug, error, info, trace, warn
 use rust_template::threads::{self, *};
 
 async fn main_async() -> ErrorCode {
@@ -25,7 +25,8 @@ fn main() -> ErrorCode {
      * WorkerGuard should be assigned in the main function or whatever the entrypoint of the program is
      * This will ensure that the guard will be dropped during an unwinding or when main exits successfully
      */
-    let _ground = logger::init();
+    let logger = Logger::default();
+    let _ground = logger.init();
     let app_info = format!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
     info!("Start {}", app_info);
     let error_code = match configs::init() {
